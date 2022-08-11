0ff0a2ba91cdb8b38297e9814c36492c55906151
zwx419685574 <419685574@qq.com>
2022-08-11 13:28:13 +0800

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+cp docker-ce.repo /etc/yum.repos.d/
+yum list docker-ce --showduplicates | sort -r
+
+yum install docker-ce-20.10.5-3.el7
+
+systemctl enable docker
+docker info | grep 'Docker Root Dir' 查看是否是更改后的路径
+
+vi /usr/lib/systemd/system/docker.service
+修改 ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock --graph /mnt/docker
+systemctl restart docker
+
