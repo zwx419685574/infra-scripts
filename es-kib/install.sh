9dcdd1a41a184f41d8c8ad00ef51bf34a4653e3f
zwx419685574 <419685574@qq.com>
2022-08-23 11:29:19 +0800

    Update install.sh

--- es-kib/install.sh
+++ es-kib/install.sh
@@ -30,6 +30,14 @@ cd filebeat-8.2.0-linux-x86_64
 vim filebeat.yml
 
 filebeat.inputs:
+- type: log
+  enabled: true     #默认为false,修改为true则启用该配置
+  paths:
+    - /home/logs/*.log
+  fields:
+    filetype: test1    #自定义字段，用来区分多个类型日志
+  fields_under_root: true    #如果该选项设置为true，则新增fields成为顶级目录，而不是将其放在fields目录下
+
 - type: filestream
   id: ftplog
   paths:
@@ -43,17 +51,26 @@ filebeat.inputs:
     service_name: vsftpd 
   # 将新增的字段放在顶级，收集后字段名称显示 host_ip。如果设置为 false，则放在子集，收集后显示为 fields.host_ip
   fields_under_root: true
-  
-- type: filestream
-  id: bussinesslog
-  paths:
-    # Filebeat处理文件的绝对路径
-    - /home/monitor/bussinesslog/*.log
-  # 使用 fields 模块添加字段
-  fields:
-    app_id: bussinesslog
-  # 将新增的字段放在顶级
-  fields_under_root: true 
+
+# ============================== Filebeat modules ==============================
+
+filebeat.config.modules:
+  # Glob pattern for configuration loading
+  path: ${path.config}/modules.d/*.yml
+
+  # Set to true to enable config reloading
+  reload.enabled: false
+
+  # Period on which files under path should be checked for changes
+  #reload.period: 10s
+
+# ======================= Elasticsearch template setting =======================
+
+setup.template.settings:
+  index.number_of_shards: 1
+  #index.codec: best_compression
+  #_source.enabled: false
+
 output.elasticsearch:
   hosts: ["localhost:9200"]
   username: "elastic"
