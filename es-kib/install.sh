8e2cd4b87b326d94026db95da0223e6bff738e91
zwx419685574 <419685574@qq.com>
2022-08-23 11:22:58 +0800

    Update install.sh

--- es-kib/install.sh
+++ es-kib/install.sh
@@ -11,7 +11,8 @@ docker run --name elasticsearch --privileged=true -p 9200:9200 -p 9300:9300 \
 -e ES_JAVA_OPTS="-Xms64m -Xmx512m" \
 -v /mnt/esdata/config:/usr/share/elasticsearch/config \
 -v /mnt/esdata/data:/usr/share/elasticsearch/data \
--v  /mnt/esdata/plugins:/usr/share/elasticsearch/plugins \
+-v /mnt/esdata/plugins:/usr/share/elasticsearch/plugins \
+-v /mnt/esdata/logs:/usr/share/elasticsearch/logs
 -d elasticsearch:8.2.0 
 
 # 根据参考链接配置es登录用户名密码
