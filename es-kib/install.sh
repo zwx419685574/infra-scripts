876972f0b9a122636403f9b7acd56552d83422c3
zwx419685574 <419685574@qq.com>
2022-09-06 11:46:10 +0800

    Update install.sh

--- es-kib/install.sh
+++ es-kib/install.sh
@@ -34,6 +34,21 @@ docker run --name kibana --restart=always \
 -p 5601:5601 \
 -d kibana:8.2.0
 
+报错：
+  A Error: [config validation of [xpack.graph].enabled]: definition for this key is missing
+  解决：配置文件里添加
+  xpack.reporting.encryptionKey: "a_random_string"
+  xpack.security.encryptionKey: "something_at_least_32_characters"
+  
+  B Error: [config validation of [elasticsearch].username]: value of "elastic" is forbidden. 
+  This is a superuser account that cannot write to system indices that Kibana needs to function. Use a service account token instead.
+  解决：在es里添加普通用户 
+      
+
+
+
+
+
 4 filebeat
 wget https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-8.2.0-linux-x86_64.tar.gz
 tar -xvf filebeat-8.2.0-linux-x86_64.tar.gz
